6abe9c5bdb37400e42e443de925231c67bab5a3b
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2025-08-09 15:32:52 -0500

    css updates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,15 @@
 // app/routes/_index.tsx (or wherever your page is)
 export default function Index() {
     return (
-        <main className="main">
-            <div className="stack">
+        <main>
+            <article>
                 <img src="/random-image" alt="Random photograph from archive" />
                 <p className="contact">
-                    Kyle Reinford ·{" "}
-                    <a href="mailto:kyledeanreinford@gmail.com">email</a> ·{" "}
+                    Kyle Reinford
+                    <a href="mailto:kyledeanreinford@gmail.com">email</a>
                     <a href="https://www.instagram.com/kdrshoots/">instagram</a>
                 </p>
-            </div>
+            </article>
         </main>
     );
 }
