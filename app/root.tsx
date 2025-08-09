f9d51c44d52bf713448ec3a8e58a87add48057a2
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2025-08-09 15:05:27 -0500

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -6,21 +6,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import resetHref from "./styles/reset.css?url";
+import siteHref from "./styles/site.css?url";
 
-import "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const links: LinksFunction = () => ([
+  { rel: "stylesheet", href: resetHref },
+  { rel: "stylesheet", href: siteHref },
+]);
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
